37dce6cfd5a72eaa7062fe0f31df782e6577e91c
sandeepmshetty <sandeepmshetty@gmail.com>
2019-11-11 20:38:49 +0530

    restructure  -- Dashboard.js

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,5 +1,12 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity, ImageBackground } from 'react-native';
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ImageBackground
+} from 'react-native';
 import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 import {createDrawerNavigator, createStackNavigator} from 'react-navigation'
 import DrawerContainer from './DrawerContainer';
@@ -7,61 +14,32 @@ import MyPage1 from './MyPage1';
 import MyPage2 from './MyPage2';
 import MyPage4 from './MyPage4';
 import MyPage5 from './MyPage5';
+import DashboardView from './Views/DashboardView';
+import styles from  './UtilComponents/main.style';
 class Dashboard extends Component {
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: '#eee', }}>
         <ImageBackground
           source={require('../assets/bg_gradient.png')}
-          style={{
-            padding: 10,
-            paddingTop: 0,
-            borderRadius: 5
-          }}>
-          <View style={styles.container}>
-            <View style={styles.card}>
-            </View>
-          </View>
+          style={styles.main}>
+              <DashboardView/>
         </ImageBackground>
-      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    margin: 30,
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
-  },
-  card: {
-    alignSelf: 'stretch',
-    backgroundColor: 'transparent',
-    borderWidth:0,
-    borderColor:'transparent'
-  }
-
-
-});
-
-
 const HomeStack = createStackNavigator({
-
   Dashboard: {
     screen: Dashboard,
-    navigationOptions: ({ navigation }) => ({
-      headerTitle: "DashBoard",
-      headerLeft: <View><TouchableOpacity onPress={() => { navigation.toggleDrawer() }}><Icon name='menu' size={35} /></TouchableOpacity></View>
+    navigationOptions: ({navigation}) => ({
+      headerTitle: "DashBoard", headerLeft: <View>
+          <TouchableOpacity
+            onPress={() => {
+            navigation.toggleDrawer()
+          }}><Icon name='menu' size={35}/></TouchableOpacity>
+        </View>
     })
-  },
-
+  }
 });
 
 const DrawerStack = createDrawerNavigator({
