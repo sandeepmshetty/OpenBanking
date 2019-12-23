5cd5dda59987e8281afb1b24db2c78a0fe7b5c69
krishnac4java <32753302+krishnac4java@users.noreply.github.com>
2019-12-23 14:36:58 +0000

    url

--- components/constants/AWSUrl.js
+++ components/constants/AWSUrl.js
@@ -1,3 +1,3 @@
 export default {
-    aws_url: 'http://openbanking-env.pt2qjnxh35.us-east-1.elasticbeanstalk.com/'
+    aws_url: 'http://openbanking-env.gvvygzzqns.us-east-1.elasticbeanstalk.com/'
   };
